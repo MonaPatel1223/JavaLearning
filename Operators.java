f86e81eac82244da3e27830bc7669ee42d5726db
MonaPatel1223 <minalthakor1223@gmail.com>
2023-02-13 21:36:21 +0000

    this is my firstjava

---
+++ Operators.java
@@ -0,0 +1,20 @@
+package numeric;
+
+public class Operators {
+    public static void main(String arg[])
+    {
+        /* int x=5;
+        int y=5;
+        System.out.println(x+y);Addition*/
+
+         int x=10;
+        int y=5;
+        System.out.println(x-y);
+
+
+
+
+
+
+    }
+}
